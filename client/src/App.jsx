d2323c6b9aaf806cb2431d4de34b3877b0137f4b
Yosvanyp74 <107805712+Yosvanyp74@users.noreply.github.com>
2023-11-12 20:49:48 +0300

    Mern app with ReactJs and Tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Header from './components/Header';
+
+export default function App() {
+  return <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/sign-in' element={<Signin/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+      </Routes>
+  </BrowserRouter>
+}
